6219ad376253a67bf09a58724b272527ba696e4b
vaibhavpotdar911 <vaibhav.potdar9946@gmail.com>
2021-05-01 20:27:17 +0530

    Background color change and CI/CD pipeline added using AWS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit, OnDestroy {
+  constructor( @Inject(DOCUMENT) private _document ) {}
+  ngOnInit() {
+      this._document.body.classList.add('bodybg-color');
+      // OR you can Add inline style css with the help of code below
+      // this._document.body.style.background = '#fff';
   }
-}
+    ngOnDestroy() {
+      // remove the class form body tag
+      this._document.body.classList.add('bodybg-color');
+    }
+  }
